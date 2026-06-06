87fd2d0eb5a28023396fa7305ff6e3108f86c72b
Ujjwal sotra <ujjwal@Ujjwals-MacBook-Air.local>
2026-06-06 17:59:37 +0530

    fixed

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message":"Hello World"}
+
+@app.get("/health")
+def getHealth():
+    return {"status":"ok"}
